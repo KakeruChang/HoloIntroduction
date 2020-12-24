8e593329f2594cfc654b5bab0d003e19846697dc
Jim <b97201019@gmail.com>
2020-12-24 21:12:14 +0800

    feat:complete 1st version of chat

--- EBookExample/View/Cell/MessageGoodImgCell.swift
+++ EBookExample/View/Cell/MessageGoodImgCell.swift
@@ -4,61 +4,37 @@
 //
 //  Created by ChangTingWei on 2020/12/23.
 //
-
-import UIKit
+import SnapKit
 
 class MessageGoodImgCell: UITableViewCell {
-
+    
     let bubble = UIView()
     let goodWrapper = UIControl()
     let good = UIImageView()
     let timeLabel = UILabel()
+    
     var isSelf: Bool = true
+    let goodDeepColor = UIColor(red: 47/255, green: 100/255, blue: 246/255, alpha: 1)
+    
+    let otherImageWrapper = UIControl()
+    let otherImage = UIImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        print("override init?")
         //
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         good.image = UIImage(systemName: "hand.thumbsup.fill")
         timeLabel.font = timeLabel.font.withSize(12)
+        goodWrapper.addTarget(self, action: #selector(touchUpGood(sender:)), for: .touchUpInside)
+        goodWrapper.addTarget(self, action: #selector(touchDownGood(sender:)), for: .touchDown)
+
         
         contentView.addSubview(bubble)
         bubble.addSubview(goodWrapper)
         goodWrapper.addSubview(good)
         bubble.addSubview(timeLabel)
         
-        bubble.translatesAutoresizingMaskIntoConstraints = false
-        let bubbleHeightConstraint = NSLayoutConstraint(item: bubble, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 60)
-        bubbleHeightConstraint.isActive = true
-        NSLayoutConstraint(item: bubble, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1, constant: -10).isActive = true
-        NSLayoutConstraint(item: bubble, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: 10).isActive = true
-        let bubbleTopConstraint = NSLayoutConstraint(item: bubble, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1, constant: 10)
-        bubbleTopConstraint.priority = UILayoutPriority(rawValue: 999)
-        bubbleTopConstraint.isActive = true
-        let bubbleBottomConstraint = NSLayoutConstraint(item: bubble, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: -10)
-        bubbleBottomConstraint.priority = UILayoutPriority(rawValue: 999)
-        bubbleBottomConstraint.isActive = true
-        
-        goodWrapper.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint(item: goodWrapper, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
-        NSLayoutConstraint(item: goodWrapper, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
-        NSLayoutConstraint(item: goodWrapper, attribute: .trailing, relatedBy: .equal, toItem: bubble, attribute: .trailing, multiplier: 1, constant: -10).isActive = true
-        NSLayoutConstraint(item: goodWrapper, attribute: .top, relatedBy: .equal, toItem: bubble, attribute: .top, multiplier: 1, constant: 10).isActive = true
-        NSLayoutConstraint(item: goodWrapper, attribute: .bottom, relatedBy: .equal, toItem: bubble, attribute: .bottom, multiplier: 1, constant: -10).isActive = true
-        
-        good.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint(item: good, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
-        NSLayoutConstraint(item: good, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
-        NSLayoutConstraint(item: good, attribute: .trailing, relatedBy: .equal, toItem: goodWrapper, attribute: .trailing, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: good, attribute: .leading, relatedBy: .equal, toItem: goodWrapper, attribute: .leading, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: good, attribute: .top, relatedBy: .equal, toItem: goodWrapper, attribute: .top, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: good, attribute: .bottom, relatedBy: .equal, toItem: goodWrapper, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
-        
-        timeLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint(item: timeLabel, attribute: .trailing, relatedBy: .equal, toItem: good, attribute: .leading, multiplier: 1, constant: 5).isActive = true
-        NSLayoutConstraint(item: timeLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 60).isActive = true
-        NSLayoutConstraint(item: timeLabel, attribute: .bottom, relatedBy: .equal, toItem: bubble, attribute: .bottom, multiplier: 1, constant: -10).isActive = true
+        initSelfCell()
     }
     
     required init?(coder: NSCoder) {
@@ -66,18 +42,95 @@ class MessageGoodImgCell: UITableViewCell {
         super.init(coder: coder)!
     }
     
+    @objc private func touchUpGood(sender: UIControl) {
+        print("good:\(timeLabel.text ?? "default")")
+        good.image = UIImage(systemName: "hand.thumbsup.fill")
+    }
+    @objc private func touchDownGood(sender: UIControl) {
+        let imageIcon = UIImage(systemName: "hand.thumbsup.fill")?.withTintColor(goodDeepColor, renderingMode: .alwaysOriginal)
+        good.image = imageIcon
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if isSelf == false {
+            setToOtherCell()
+        } else {
+            setToSelfCell()
+        }
+        contentView.layoutIfNeeded()
+    }
+    
+    func initSelfCell(){
+        bubble.snp.remakeConstraints { (make) -> Void in
+            make.height.equalTo(60)
+            make.left.right.equalToSuperview().inset(10)
+            make.top.bottom.equalToSuperview().inset(10).priority(999)
+        }
+
+        setToSelfCell()
+    }
+    
+    func setToSelfCell(){
+        otherImageWrapper.removeFromSuperview()
+        
+        goodWrapper.snp.remakeConstraints { (make) -> Void in
+            make.height.width.equalTo(40)
+            make.right.top.bottom.equalToSuperview().inset(10)
+        }
+
+        good.snp.remakeConstraints { (make) -> Void in
+            make.height.width.equalTo(40)
+            make.left.right.top.bottom.equalToSuperview()
+        }
+
+        timeLabel.snp.remakeConstraints { (make) -> Void in
+            make.width.equalTo(60)
+            make.right.equalTo(goodWrapper.snp.left).inset(5)
+            make.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    func setToOtherCell(){
+        otherImage.image = UIImage(systemName: "person.fill")
+        bubble.addSubview(otherImageWrapper)
+        otherImageWrapper.addSubview(otherImage)
+        
+        otherImageWrapper.snp.remakeConstraints { (make) -> Void in
+            make.width.height.equalTo(40)
+            make.bottom.top.equalToSuperview().inset(10)
+            make.left.equalToSuperview().inset(0)
+        }
+        
+        otherImage.snp.remakeConstraints { (make) -> Void in
+            make.width.height.equalTo(40)
+            make.bottom.left.right.top.equalToSuperview()
+        }
+        
+        goodWrapper.snp.remakeConstraints { (make) -> Void in
+            make.height.width.equalTo(40)
+            make.left.equalTo(otherImageWrapper.snp.right).offset(10)
+            make.bottom.equalToSuperview().inset(10)
+        }
+
+        timeLabel.snp.remakeConstraints { (make) -> Void in
+            make.width.equalTo(60)
+            make.left.equalTo(goodWrapper.snp.right).offset(5)
+            make.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-print("test")
-
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
-
